14f9359dd35ccb7aa61aa2a5015e9697c8858d15
Brian Bowman <seeker14491@gmail.com>
2021-09-21 16:44:07 -0500

    Update dumper
    - Simplify commandline flags
    - Support writing Bytes (pass-through)

--- distance-bytes-dumper/src/cli_args.rs
+++ distance-bytes-dumper/src/cli_args.rs
@@ -1,19 +1,19 @@
-use std::path::PathBuf;
+use clap::arg_enum;
 use structopt::StructOpt;
+
+arg_enum! {
+    #[derive(Debug)]
+    pub enum OutputFormat {
+        Json,
+        Yaml,
+        Bytes,
+    }
+}
 
 #[derive(Debug, StructOpt)]
 pub struct Opt {
-    #[structopt(parse(from_os_str))]
-    pub bytes_file: Option<PathBuf>,
-
-    #[structopt(long)]
-    pub json: bool,
-
-    #[structopt(long)]
-    pub yaml: bool,
-
-    #[structopt(short, long, parse(from_os_str))]
-    pub output: Option<PathBuf>,
+    #[structopt(long, possible_values = &OutputFormat::variants(), case_insensitive = true)]
+    pub format: OutputFormat,
 }
 
 pub fn get() -> Opt {
